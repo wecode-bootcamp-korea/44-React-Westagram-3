2b12213f2f261ba113d036111c77e5321527649a
박지연 <parkjiyeoun@bagjiyeon-ui-MacBookPro.local>
2023-03-22 20:13:18 +0900

    [3팀]추가세팅수정완료

--- src/ProductDetail.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ProductDetail = () => {
-  return <img src="/images/PruductDetail/profile04.jpg" alt="프로덕트1" />;
-};
-
-export default ProductDetail;
--- src/Router.js
+++ src/Router.js
@@ -8,6 +8,7 @@ import MainCho from './pages/chogeonho/Main/Main';
 // 박지연의 컴포넌트
 import LoginPark from './pages/parkjiyeoun/Login/Login';
 import MainPark from './pages/parkjiyeoun/Main/Main';
+import Nav from './components/Nav/Nav';
 
 // import 한 컴포넌트에 대한 경로를 각각 설정해줍니다.
 const Router = () => {
@@ -18,6 +19,7 @@ const Router = () => {
         <Route path="/main-cho" element={<MainCho />} />
         <Route path="/login-park" element={<LoginPark />} />
         <Route path="/main-park" element={<MainPark />} />
+        <Route path="/nav" element={<Nav />} />
       </Routes>
     </BrowserRouter>
   );
